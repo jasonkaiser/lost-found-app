5d0aaa9f2a54a66d99056944b12721d660e13a19
Jason Kaiser <alexander.jason.kaiser@stu.ibu.edu.ba>
2025-07-24 14:49:43 +0200

    Deployment Details

--- backend/public/v1/docs/swagger.php
+++ backend/public/v1/docs/swagger.php
@@ -2,12 +2,12 @@
 require __DIR__ . '/../../../vendor/autoload.php';
 
 define('LOCALSERVER', 'http://localhost/fullstack-web-app/backend/');
-define('PRODSERVER', 'https://lost-found-2aizc.ondigitalocean.app/api/');
+define('PRODSERVER', 'https://lost-found-973uv.ondigitalocean.app/api/');
 
 if($_SERVER['SERVER_NAME'] == 'localhost' || $_SERVER['SERVER_NAME'] == '127.0.0.1'){
     define('BASE_URL', 'http://localhost/fullstack-web-app/backend/');
 } else {
-    define('BASE_URL', 'https://lost-found-2aizc.ondigitalocean.app/api/');
+    define('BASE_URL', 'https://lost-found-973uv.ondigitalocean.app/api/');
 }
 
 $openapi = \OpenApi\Generator::scan([
