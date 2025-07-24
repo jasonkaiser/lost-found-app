48ec9bf932c8462b3da72d6d2937688b74fec7e9
Jason Kaiser <alexander.jason.kaiser@stu.ibu.edu.ba>
2025-07-24 18:05:10 +0200

    Deployment

--- frontend/utils/rest-client.js
+++ frontend/utils/rest-client.js
@@ -3,7 +3,7 @@ let Constants = {
         if(location.hostname === 'localhost' || location.hostname === '127.0.0.1') {
             return '/lost-found-app/backend/';
         } else {
-            return 'https://lost-found-973uv.ondigitalocean.app/api/';
+            return 'https://lost-found-973uv.ondigitalocean.app/backend/';
         }
     },
     USER_ROLE: "User",
